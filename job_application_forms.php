1a403cfdecb371da2fbde90a9392637f77abbb6d
pinkth3floyd <prakash@zeptosky.com>
2025-09-04 16:58:14 +0545

    asdf

--- job_application_forms.php
+++ job_application_forms.php
@@ -438,7 +438,7 @@ function job_application_form_shortcode() {
                         </div>
                         <div>
                             <label class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" for="nationality">Nationality</label>
-                            <input class="flex h-9 w-full rounded-md border border-gray-300 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-indigo-500 disabled:cursor-not-allowed disabled:opacity-50" id="nationality" placeholder="Enter your nationality" value="Australian" name="nationality">
+                            <input class="flex h-9 w-full rounded-md border border-gray-300 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-indigo-500 disabled:cursor-not-allowed disabled:opacity-50" id="nationality" placeholder="Enter your nationality" value="Nepalese" name="nationality">
                         </div>
                     </div>
                 </div>
